b7ba7bee44718fe540112fe5badd75565c81ab48
nitesh solanki <nitesh7sid@gmail.com>
2017-08-11 15:51:51 +0530

    bug fixes

--- chaincode/helloWorld/helloWorld.go
+++ chaincode/helloWorld/helloWorld.go
@@ -1,39 +1,12 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-/*
- * The sample smart contract for documentation topic:
- * Writing Your First Blockchain Application
- */
-
 package main
 
 /* Imports
- * 4 utility libraries for formatting, handling bytes, reading and writing JSON, and string manipulation
+ * 1 utility libraries for formatting
  * 2 specific Hyperledger Fabric specific libraries for Smart Contracts
  */
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"strconv"
 
+	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	sc "github.com/hyperledger/fabric/protos/peer"
 )
@@ -42,12 +15,14 @@ import (
 type SmartContract struct {
 }
 
+var logger = shim.NewLogger("hello_world")
 
 /*
  * The Init method is called when the Smart Contract "fabcar" is instantiated by the blockchain network
  * Best practice is to have any Ledger initialization in separate function -- see initLedger()
  */
 func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	logger.Info("########### hello_world Init ###########")
 	return shim.Success(nil)
 }
 
@@ -57,36 +32,44 @@ func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
  */
 func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
 
+	logger.Info("########### hello_world Invoke ###########")
 	// Retrieve the requested Smart Contract function and arguments
 	function, args := APIstub.GetFunctionAndParameters()
 	// Route to the appropriate handler function to interact with the ledger appropriately
-	if function == "query" {
-		return s.query(APIstub, args)
-	} else if function == "invoke" {
-		return s.invoke(APIstub)
+	if function == "readFunc1" {
+		return s.readFunc1(APIstub, args)
+	} else if function == "writeFunc1" {
+		return s.writeFunc1(APIstub,args)
 	}
 
+logger.Errorf("Unknown action, check the first argument, must be one of 'readFunc1', 'writeFunc1'. But got: %v", args[0])
 	return shim.Error("Invalid Smart Contract function name.")
 }
 
-func (s *SmartContract) query(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+func (s *SmartContract) readFunc1(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
+	logger.Infof("key = %d", args[0])
+
 	valAsBytes, _ := APIstub.GetState(args[0])
+
+	logger.Infof("Query Response:%s\n", valAsBytes)
 	return shim.Success(valAsBytes)
 }
 
-func (s *SmartContract) invoke(APIstub shim.ChaincodeStubInterface,args []string) sc.Response {
+func (s *SmartContract) writeFunc1(APIstub shim.ChaincodeStubInterface,args []string) sc.Response {
+
+	logger.Infof("key = %d, value = %d\n", args[0], args[1])
 
 		APIstub.PutState(args[0], []byte(args[1]))
 
 		return shim.Success(nil)
 }
 
-// The main function is only relevant in unit test mode. Only included here for completeness.
+
 func main() {
 
 	// Create a new Smart Contract
